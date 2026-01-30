388b401276345c7a4c1f5bfa1700845dad6bb551
n1kolay-zhurkin <zhurkun96@gmail.com>
2026-01-30 23:58:15 +0300

    Добавлена fixture driver для Selenium

--- conftest.py
+++ conftest.py
@@ -1,1 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(0)
+    yield driver
+    driver.quit()
